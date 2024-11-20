dcb13d73c040237d886f68f3f6ac8e98386ea8c4
Samuel Mashil <69389564+TangyPenguin37@users.noreply.github.com>
2024-11-20 11:40:45 +0000

    Download video from web

--- test.py
+++ test.py
@@ -1,7 +1,24 @@
-print("hello!")
+import os
+import requests
+import tqdm
 
-print("hello2")
 
-x = 1
+def getVideo():
+    # url = "https://cdn.pixabay.com/video/2022/11/22/140111-774507949_large.mp4" # - seagulls
+    url = "https://cdn.pixabay.com/video/2023/10/12/184734-873923034_large.mp4"  # - car
 
-print(x)
+    # Perform a get on the url
+    response = requests.get(url, timeout=30)
+
+    # Write the content to a file
+    if os.path.exists("video.mp4"):
+        os.remove("video.mp4")
+
+    with open("video.mp4", "wb") as file:
+        file.write(response.content)
+
+    print("Video downloaded successfully")
+
+
+if __name__ == "__main__":
+    getVideo()
